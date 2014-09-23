d1039c433d4a70a0a102b0a295b5332f714d1f56
endermcbane <kccrookston@gmail.com>
2014-09-22 23:18:30 -0600

    v1

--- js/app.js
+++ js/app.js
@@ -12,6 +12,97 @@ $(document).ready(function(){
   		$(".overlay").fadeOut(1000);
   	});
 
+  	var secretnum;
+	var count = 0	
+
+  	var newgame = function() {
+  		secretnum = Math.floor(Math.random() * (100 - 1 + 1)) + 1;
+  		$('#guessList').children().remove();
+  		$('#feedback').html("Make Your Guess");
+  		$('#feedback').append(secretnum);
+  		count = 0
+  		$('#count').html(count);
+  		return secretnum;
+  	};
+
+  	newgame();
+  	
+	function check(){
+		var guess = $('#userGuess').val();
+		count++
+		$('#count').html(count);
+		$('#guessList').append('<li>' + guess + '</li>');
+		var difference = Math.abs(guess-secretnum);
+		$('#userGuess').val("");
+		
+		if (difference == 0) {
+			$('#feedback').html("Correct!");
+			
+		}
+		else if (difference < 10) {
+			$('#feedback').html("Very Hot");
+			
+		}
+
+		else if (difference < 20) {
+			$('#feedback').html("Hot");
+			
+		}
+
+		else if (difference < 30) {
+			$('#feedback').html("Wam");
+			
+		}
+
+		else if (difference < 50) {
+			$('#feedback').html("Cold");
+			
+		}
+
+		else if (difference >= 50) {
+			$('#feedback').html("Ice Cold");
+			
+		}
+		else {
+			$('#feedback').html("Something Broke");
+		};
+
+		if (secretnum == guess){
+			$('#feedback').append('!');
+		}
+		else if (secretnum > guess){
+			$('#feedback').append('<p>(guess higher)</p>');
+		}
+		else if (secretnum < guess){
+			$('#feedback').append('<p>(guess lower)</p>');
+		}
+		else {
+			$('#feedback').append("Something Broke");
+		}
+		
+		
+	};
+
+
+	$("#guessButton").click(function(e) {
+		e.preventDefault(e);
+		if (($('#userGuess').val()) > 100 || ($('#userGuess').val()) < 0 || ($('#userGuess').val()) % 1 !=0 ){
+			alert('Make sure you enter a whole number between 1 and 100');
+			$('#userGuess').val("");
+		}
+		else{
+		check()
+		};
+	});
+
+	$(".new").click(function(e) {
+		e.preventDefault(e);
+		newgame();
+	});
+
+	
+
+
 });
 
 
